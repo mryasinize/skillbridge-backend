ddf95695575e4b1df642976a0a6f37f83740ed06
Yasin Ahmed <myselfyasinahmed@gmail.com>
2026-01-30 00:34:53 +0600

    feat: added category in booking

--- src/modules/bookings/booking.schema.ts
+++ src/modules/bookings/booking.schema.ts
@@ -5,7 +5,8 @@ export const BookingSchema = z.object({
     studentId: z.uuid(),
     tutorProfileId: z.uuid(),
     startTime: z.coerce.date(),
-    endTime: z.coerce.date()
+    endTime: z.coerce.date(),
+    categoryId: z.uuid()
 }).refine(data => data.startTime < data.endTime, {
     error: "startTime must be before endTime"
 })
