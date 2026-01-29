f24ef761b16ebc97b8d4dc812b1c1b30aa3446db
Yasin Ahmed <myselfyasinahmed@gmail.com>
2026-01-29 21:19:56 +0600

    feat: added middleware to check role

---
+++ src/middlewares/checkRole.ts
@@ -0,0 +1,10 @@
+import type { NextFunction, Request, Response } from "express";
+import type { Role } from "../generated/prisma/enums";
+import { ApiError } from "../utils/ApiError";
+
+export const authenticate = (role: keyof typeof Role) => (req: Request, res: Response, next: NextFunction) => {
+    if (req.user!.role !== role) {
+        throw new ApiError(403, "Forbidden. You don't have sufficient permission to perform this action")
+    }
+    next()
+};
